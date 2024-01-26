f00c1b3c29fecda5f6d2c541ead6b3eb8e33459c
bidianqing <bidianqing@longfor.com>
2024-01-26 17:45:59 +0800

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/demo?charset=utf8mb4")
+	if err != nil {
+		fmt.Printf("Open failed, err:%v\n", err)
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("Ping failed, err:%v\n", err)
+		return
+	}
+
+	result, err := db.Exec(`insert into tb_menu(Name,ParentId,Icon,Path) values ('2.3', 2, null, null)`)
+	if err != nil {
+		fmt.Printf("Exec failed, err:%v\n", err)
+		return
+	}
+
+	fmt.Println(result)
+}
